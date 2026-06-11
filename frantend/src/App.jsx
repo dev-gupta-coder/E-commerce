ff7f32633c04da359fdef7abc179db9ce406dc3a
dev-gupta-coder <dev45214521@example.com>
2026-06-11 18:03:29 +0530

    remove garbage frontend folder

--- frantend/src/App.jsx
+++
@@ -1,9 +0,0 @@
-export default function App() {
-  return (
-    <div className="min-h-screen bg-slate-900 text-white flex items-center justify-center">
-      <h1 className="text-5xl font-bold">
-        Tailwind 4 Working 🚀
-      </h1>
-    </div>
-  );
-}
